9e08a069c87a24ec1acc44e78c922d786762f5fa
FaizanK001 <47453699+FaizanK001@users.noreply.github.com>
2021-08-05 00:08:38 +0100

    First Commit

---
+++ Checkout/src/check_out/Check_Out.java
@@ -0,0 +1,43 @@
+package check_out;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Check_Out {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Check_Out window = new Check_Out();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Check_Out() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 1370, 700);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
